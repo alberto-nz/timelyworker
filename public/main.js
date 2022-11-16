2acfa828008299f4b969d6f42254ca090f390098
Alberto <akanezks@gmail.com>
2022-11-16 19:50:28 +0100

    add base auth with firebase

--- public/main.js
+++ public/main.js
@@ -1,7 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loadEl = document.querySelector('#load');
-   
-     firebase.database().ref('usuarios').on('value', snapshot => {const data = snapshot.val()
-      console.log(data) });
-  
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  firebase
+    .database()
+    .ref("usuarios")
+    .on("value", snapshot => {
+      const data = snapshot.val();
+      console.log(data);
+      const email = document.getElementById("email");
+      const name = document.getElementById("name");
+      data.forEach(element => {
+        email.innerHTML += `<li>${element.email}</li>`;
+        name.innerHTML += `<li>${element.first_name} ${element.last_name}</li>`;
+      });
+    });
+});
